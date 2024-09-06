beb37c64c17218743976f240d45061e3a43ab815
HatayErincErdemli <hatayerinc@gmail.com>
2024-09-06 04:26:39 +0300

    ilk test eklendi

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,14 @@
-describe('My First Test', () => {
-  it('Does not do much!', () => {
-    expect(true).to.equal(true)
+describe('login test', () => {
+  beforeEach(() => {
+  
+  cy.visit('http://localhost:5173/')
+})
+ 
+  it('başarılı form submit edilebiliyor', () => {
+    cy.get("[data-cy=email]").type("erdem.guntay@wit.com.tr")
+    cy.get("[data-cy=password]").type("9fxIH0GXesEwH_I")
+    cy.get("[data-cy=terms]").check()
+    cy.get("[data-cy=signin]").click()
+    cy.url().should("include", "/main")    
   })
 })
